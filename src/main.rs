ef55abf5f08d70aff9cda0feaf17aaa25b39e463
Sodment <sodment@gmail.com>
2020-12-12 11:22:24 +0100

    FirstCommit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+//mod city;
+pub mod file_reader;
+pub mod brute_force_tsp;
+pub mod city;
+
+fn main() {
+    //let cities : Vec<city::City> = city::City::random_cities(21, 0, 120);
+    let read_file = file_reader::read_file("C:\\users\\public\\ber52.txt");
+    let cities  = city::City::string_to_city(read_file);
+    println!("{}", brute_force_tsp::greedy_way(&cities));
+}
+
